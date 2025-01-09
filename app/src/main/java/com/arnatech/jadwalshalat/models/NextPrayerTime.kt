34c4f68690aa280f8449589d7fbba99eedc0aefa
SenbiDev <senbidev@gmail.com>
2025-01-09 15:53:58 +0700

    add: add model class

---
+++ app/src/main/java/com/arnatech/jadwalshalat/models/NextPrayerTime.kt
@@ -0,0 +1,6 @@
+package com.arnatech.jadwalshalat.models
+
+import android.widget.TextView
+import kotlinx.datetime.Instant
+
+class NextPrayerTime(val prayerTime: Instant, val prayerCountdownText: TextView)
