5ebc42e3cfdf46c2ef26b3674208350a4c5f4fe7
SenbiDev <senbidev@gmail.com>
2025-02-18 08:19:10 +0700

    modif: change logic of ImageSliderAdapter.kt

--- app/src/main/java/com/arnatech/jadwalshalat/ImageSliderAdapter.kt
+++ app/src/main/java/com/arnatech/jadwalshalat/ImageSliderAdapter.kt
@@ -5,22 +5,33 @@ import android.view.animation.AlphaAnimation
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.arnatech.jadwalshalat.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
-class ImageSliderAdapter(private val imageList: List<Int>) :
-    RecyclerView.Adapter<ImageSliderAdapter.ImageViewHolder>() {
+class ImageSliderAdapter(private val imageList: List<String>) :
+    RecyclerView.Adapter<ImageSliderAdapter.BackgroundImageViewHolder>() {
 
-    inner class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class BackgroundImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val imageView: ImageView = view.findViewById(R.id.imageView)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BackgroundImageViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_slide, parent, false)
-        return ImageViewHolder(view)
+        return BackgroundImageViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-        holder.imageView.setImageResource(imageList[position])
+    override fun onBindViewHolder(holder: BackgroundImageViewHolder, position: Int) {
+//        holder.imageView.setImageResource(imageList[position])
+        val image = imageList[position]
+        Glide.with(holder.itemView.context)
+            .load(image)
+            .apply(
+                RequestOptions
+                    .placeholderOf(R.drawable.ic_loading)
+                    .error(R.drawable.ic_error)
+            )
+            .into(holder.imageView)
 
         val fadeIn = AlphaAnimation(0f, 1f)
         fadeIn.duration = 1000
