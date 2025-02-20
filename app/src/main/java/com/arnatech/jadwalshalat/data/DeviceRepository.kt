2f2dbb3a60ed419f45957eaaf9ffd32656ddb346
SenbiDev <senbidev@gmail.com>
2025-02-20 14:48:26 +0700

    modif: change some DeviceRepository logic

--- app/src/main/java/com/arnatech/jadwalshalat/data/DeviceRepository.kt
+++ app/src/main/java/com/arnatech/jadwalshalat/data/DeviceRepository.kt
@@ -5,9 +5,21 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.liveData
 import com.arnatech.jadwalshalat.data.local.db.AppDatabase
+import com.arnatech.jadwalshalat.data.local.table.BackgroundImageEntity
+import com.arnatech.jadwalshalat.data.local.table.ConfigurationTable
 import com.arnatech.jadwalshalat.data.local.table.DeviceTable
+import com.arnatech.jadwalshalat.data.local.table.MosqueTable
+import com.arnatech.jadwalshalat.data.local.table.PrayerScheduleTable
+import com.arnatech.jadwalshalat.data.local.table.SlidersTable
+import com.arnatech.jadwalshalat.data.local.table.TextMarqueeTable
 import com.arnatech.jadwalshalat.data.remote.api.ApiService
-import com.arnatech.jadwalshalat.data.remote.response.DeviceResponse
+import com.arnatech.jadwalshalat.models.BackgroundImage
+import com.arnatech.jadwalshalat.models.Configuration
+import com.arnatech.jadwalshalat.models.DeviceData
+import com.arnatech.jadwalshalat.models.Mosque
+import com.arnatech.jadwalshalat.models.PrayerSchedule
+import com.arnatech.jadwalshalat.models.Slider
+import com.arnatech.jadwalshalat.models.TextMarquee
 import java.util.UUID
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -18,6 +30,12 @@ class DeviceRepository private constructor(
     appDatabase: AppDatabase
 ) {
     private val deviceDao = appDatabase.deviceDao()
+    private val mosqueDao = appDatabase.mosqueDao()
+    private val prayerScheduleDao = appDatabase.prayerScheduleDao()
+    private val sliderDao = appDatabase.sliderDao()
+    private val textMarqueeDao = appDatabase.textMarqueeDao()
+    private val configurationDao = appDatabase.configurationDao()
+
     private val api = apiService
     private val dummyApi = dummyApiService
 
@@ -35,35 +53,118 @@ class DeviceRepository private constructor(
         }
     }
 
-    fun getDeviceData(deviceId: String): LiveData<Result<DeviceResponse>> = liveData {
+    fun getDeviceData(deviceId: String): LiveData<Result<DeviceData>> = liveData {
         emit(Result.Loading)
-        try {
-            //            val articles = response.articles
-//            val newsList = articles.map { article ->
-//                val isBookmarked = newsDao.isNewsBookmarked(article.title)
-//                NewsEntity(
-//                    article.title,
-//                    article.publishedAt,
-//                    article.urlToImage,
-//                    article.url,
-//                    isBookmarked
-//                )
-//            }
-//            newsDao.deleteAll()
-//            newsDao.insertNews(newsList)
 
-//            val response = dummyApi.getContentByDeviceId(deviceId)
-            val response = api.getContentByDeviceId(deviceId)
-//            Log.i("HASIL:", response.mosque?.name ?: "NOL")
-//            Log.i("HASIL JADWAL:", response.prayerSchedule?.fajr ?: "NOL")
-            val dummyResponse: LiveData<Result<DeviceResponse>> = MutableLiveData(Result.Success(response))
-            emitSource(dummyResponse)
+        // FETCH & SAVE TO DB
+        try {
+            val response = dummyApi.getContentByDeviceId(deviceId)
+//            val response = api.getContentByDeviceId(deviceId)
+            val mosqueTable = MosqueTable(
+                0,
+                response.mosque?.name,
+                response.mosque?.address,
+                response.mosque?.latitude.toString().toDoubleOrNull(),
+                response.mosque?.longitude.toString().toDoubleOrNull(),
+            )
+            val prayerScheduleList = response.prayerSchedule?.map { schedule ->
+                PrayerScheduleTable(
+                    0,
+                    schedule?.date,
+                    schedule?.asr,
+                    schedule?.sunrise,
+                    schedule?.isha,
+                    schedule?.dhuhr,
+                    schedule?.fajr,
+                    schedule?.maghrib,
+                )
+            } ?: listOf()
+            val sliderList = response.sliders?.map { sliderItem ->
+                SlidersTable(
+                    sliderItem?.id,
+                    sliderItem?.mosque,
+                    BackgroundImageEntity(
+                        sliderItem?.backgroundImage?.id,
+                        sliderItem?.backgroundImage?.name,
+                        sliderItem?.backgroundImage?.url,
+                        sliderItem?.backgroundImage?.fileSize
+                    ),
+                    sliderItem?.text,
+                    sliderItem?.createdAt
+                )
+            } ?: listOf()
+            val textMarqueeList = response.textMarquee?.map { textMarquee ->
+                TextMarqueeTable(
+                    textMarquee?.id,
+                    textMarquee?.mosque,
+                    textMarquee?.createdAt,
+                    textMarquee?.text,
+                )
+            } ?: listOf()
+            val configurationTable = ConfigurationTable(
+                0,
+                response.configurations?.mosque,
+                response.configurations?.maxSliders,
+                response.configurations?.maxTextMarquee,
+                response.configurations?.prayerDurationDays,
+                response.configurations?.allowCalendarAccess,
+                response.configurations?.createdAt,
+            )
+            mosqueDao.deleteAll()
+            prayerScheduleDao.deleteAll()
+            sliderDao.deleteAll()
+            textMarqueeDao.deleteAll()
+            configurationDao.deleteAll()
+
+            mosqueDao.insertMosque(mosqueTable)
+            prayerScheduleDao.insertPrayerScheduleList(prayerScheduleList)
+            sliderDao.insertSlidersList(sliderList)
+            textMarqueeDao.insertTextMarqueeList(textMarqueeList)
+            configurationDao.insertConfiguration(configurationTable)
         } catch (e: Exception) {
-            Log.d("NewsRepository", "getHeadlineNews: ${e.message.toString()} ")
+            Log.d("DeviceRepository", "getDeviceData: ${e.message.toString()} ")
             emit(Result.Error(e.message.toString()))
         }
-//        val localData: LiveData<Result<List<NewsEntity>>> = newsDao.getNews().map { Result.Success(it) }
-//        emitSource(localData)
+
+        // GET FROM DB
+        val localMosque = mosqueDao.getMosque()?.run {
+            Mosque(id, name, address, latitude, longitude)
+        }
+        val localPrayerScheduleList = prayerScheduleDao.getAllPrayerSchedule().map {
+            it.run { PrayerSchedule(date, asr, sunrise, isha, dhuhr, fajr, maghrib) }
+        }
+        val localSliderList = sliderDao.getAllSliders().map {
+            it.run {
+                Slider(
+                    id,
+                    mosque,
+                    BackgroundImage(
+                        backgroundImage?.backgroundId,
+                        backgroundImage?.name,
+                        backgroundImage?.url,
+                        backgroundImage?.fileSize
+                    ),
+                    text,
+                    createdAt
+                )
+            }
+        }
+        val localTextMarqueeList = textMarqueeDao.getAllTextMarquee().map {
+            it.run { TextMarquee(mosque, createdAt, id, text) }
+        }
+        val localConfiguration = configurationDao.getConfiguration()?.run {
+            Configuration(id, mosque, maxSliders, maxTextMarquee, prayerDurationDays, allowCalendarAccess, createdAt)
+        }
+
+        val deviceData = DeviceData(
+            localMosque,
+            localPrayerScheduleList,
+            localSliderList,
+            localTextMarqueeList,
+            localConfiguration
+        )
+        val localDeviceData: LiveData<Result<DeviceData>> = MutableLiveData(Result.Success(deviceData))
+        emitSource(localDeviceData)
     }
 
     companion object {
