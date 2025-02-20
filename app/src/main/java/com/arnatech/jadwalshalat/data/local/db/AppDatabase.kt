23241f9b194bb2c20e9550725447f179bf82ade8
SenbiDev <senbidev@gmail.com>
2025-02-20 14:48:04 +0700

    modif: change some db config

--- app/src/main/java/com/arnatech/jadwalshalat/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/arnatech/jadwalshalat/data/local/db/AppDatabase.kt
@@ -4,12 +4,38 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.arnatech.jadwalshalat.data.local.db.dao.ConfigurationDao
 import com.arnatech.jadwalshalat.data.local.table.DeviceTable
 import com.arnatech.jadwalshalat.data.local.db.dao.DeviceDao
+import com.arnatech.jadwalshalat.data.local.db.dao.MosqueDao
+import com.arnatech.jadwalshalat.data.local.db.dao.PrayerScheduleDao
+import com.arnatech.jadwalshalat.data.local.db.dao.SliderDao
+import com.arnatech.jadwalshalat.data.local.db.dao.TextMarqueeDao
+import com.arnatech.jadwalshalat.data.local.table.ConfigurationTable
+import com.arnatech.jadwalshalat.data.local.table.MosqueTable
+import com.arnatech.jadwalshalat.data.local.table.PrayerScheduleTable
+import com.arnatech.jadwalshalat.data.local.table.SlidersTable
+import com.arnatech.jadwalshalat.data.local.table.TextMarqueeTable
 
-@Database(entities = [DeviceTable::class], version = 1, exportSchema = false)
+@Database(
+    entities = [
+        DeviceTable::class,
+        MosqueTable::class,
+        PrayerScheduleTable::class,
+        SlidersTable::class,
+        TextMarqueeTable::class,
+        ConfigurationTable::class
+               ],
+    version = 2,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun deviceDao(): DeviceDao
+    abstract fun mosqueDao(): MosqueDao
+    abstract fun prayerScheduleDao(): PrayerScheduleDao
+    abstract fun sliderDao(): SliderDao
+    abstract fun textMarqueeDao(): TextMarqueeDao
+    abstract fun configurationDao(): ConfigurationDao
 
     companion object {
         @Volatile
@@ -21,7 +47,8 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "device_database"
-                ).build()
+                ).fallbackToDestructiveMigration()
+                    .build()
                 INSTANCE = instance
                 instance
             }
