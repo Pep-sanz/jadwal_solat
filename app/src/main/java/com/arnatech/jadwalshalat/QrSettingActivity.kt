31976057d2b3e6819c00ebae36431aff80a8d05a
SenbiDev <senbidev@gmail.com>
2025-02-21 10:36:01 +0700

    modif: add handler for refresh button event

--- app/src/main/java/com/arnatech/jadwalshalat/QrSettingActivity.kt
+++ app/src/main/java/com/arnatech/jadwalshalat/QrSettingActivity.kt
@@ -3,12 +3,16 @@ package com.arnatech.jadwalshalat
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.arnatech.jadwalshalat.databinding.ActivityQrSettingBinding
 import com.arnatech.jadwalshalat.factory.ViewModelFactory
+import com.arnatech.jadwalshalat.sharedviewmodel.PrayerScheduleApplication
 import com.arnatech.jadwalshalat.utils.QRCodeGenerator
 import com.arnatech.jadwalshalat.viewmodel.DeviceViewModel
 
@@ -18,6 +22,8 @@ class QrSettingActivity : AppCompatActivity() {
 
     private lateinit var qrImageView: ImageView
     private lateinit var deviceIdTextView: TextView
+    private lateinit var refreshButton: Button
+    private lateinit var localDeviceId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,15 +35,30 @@ class QrSettingActivity : AppCompatActivity() {
 
         deviceIdTextView = binding.deviceIdTextView
         qrImageView = binding.qrImageView
+        refreshButton = binding.refreshButton
 
-        val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
-        val viewModel: DeviceViewModel = ViewModelProvider(this, factory)[DeviceViewModel::class.java]
+        val factory = (application as PrayerScheduleApplication).getViewModelFactory()
+        val viewModel: DeviceViewModel = ViewModelProvider(application as PrayerScheduleApplication, factory)[DeviceViewModel::class.java]
+
         viewModel.getOrCreateDeviceId()
         viewModel.deviceId.observe(this) { deviceId ->
             Log.i("Device ID", deviceId)
+            localDeviceId = deviceId
             deviceIdTextView.text = "Device ID: $deviceId"
             val qrCodeBitmap: Bitmap = QRCodeGenerator.generateQRCode(deviceId, 512, 512)
             qrImageView.setImageBitmap(qrCodeBitmap)
+            refreshButton.visibility = View.VISIBLE
+        }
+
+        refreshButton.requestFocus()
+        refreshButton.setOnClickListener {
+            Log.i("AMBIL", localDeviceId)
+            viewModel.getDeviceData(this, localDeviceId)
+            Toast.makeText(
+                this,
+                "Refreshing data...",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
