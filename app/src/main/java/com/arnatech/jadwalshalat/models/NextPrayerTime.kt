c997a35e69032217ed851b71eefe6b93de987631
SenbiDev <senbidev@gmail.com>
2025-02-22 09:26:58 +0700

    modif: add prayerName field at NextPrayerTime.kt

--- app/src/main/java/com/arnatech/jadwalshalat/models/NextPrayerTime.kt
+++ app/src/main/java/com/arnatech/jadwalshalat/models/NextPrayerTime.kt
@@ -3,4 +3,4 @@ package com.arnatech.jadwalshalat.models
 import android.widget.TextView
 import kotlinx.datetime.Instant
 
-class NextPrayerTime(val prayerTime: Instant, val prayerCountdownText: TextView)
+class NextPrayerTime(val prayerName: String, val prayerTime: Instant, val prayerCountdownText: TextView)
